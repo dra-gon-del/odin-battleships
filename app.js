2ed33c044f5e242883849c2957872727abc2c5cc
A.Westwood <personal.email.aw1@gmail.com>
2024-11-07 19:05:14 +0000

    12. write js for gameboard.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container');
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
 
@@ -7,8 +8,19 @@ function flip() {
     const optionShips = Array.from(optionContainer.children);
     angle = angle === 0 ? 90 : 0;
     optionShips.forEach(optionShip =>optionShip.style.transform = `rotate(${angle}deg)`);
-
 };
 
 // Creating Boards
+const width = 10;
+
+function createBoard() {
+    const gameBoardContainer = document.createElement('div');
+    gameBoardContainer.classList.add('game-board');
+    gameBoardContainer.style.backgroundColor = 'pink';
+
+    gamesBoardContainer.append(gameBoardContainer);
+};
+
+createBoard();
+
 flipButton.addEventListener('click', flip); 
