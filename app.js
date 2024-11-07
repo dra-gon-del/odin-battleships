8827453c27f1e780d2f9521154f6cb70e95a6043
A.Westwood <personal.email.aw1@gmail.com>
2024-11-07 19:15:21 +0000

    13. style board and createBoard x2.

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ function flip() {
 // Creating Boards
 const width = 10;
 
-function createBoard() {
+function createBoard(color) {
     const gameBoardContainer = document.createElement('div');
     gameBoardContainer.classList.add('game-board');
-    gameBoardContainer.style.backgroundColor = 'pink';
+    gameBoardContainer.style.backgroundColor = color;
 
     gamesBoardContainer.append(gameBoardContainer);
 };
 
-createBoard();
+createBoard('yellow', 'player');
+createBoard('pink', 'computer');
 
 flipButton.addEventListener('click', flip); 
