15ab9301b2c2037b56ed9b2d50a195bd87315d99
A.Westwood <personal.email.aw1@gmail.com>
2024-11-18 15:02:24 +0000

    19. Write functions that prevent overlapping and splitting ships.

--- app.js
+++ app.js
@@ -55,22 +55,45 @@ function addShipPiece(ship) {
     let randomBoolean = Math.random() < 0.5;
     let isHorizontal = randomBoolean;
     let randomStartIndex = Math.floor(Math.random() * width * width);
-    console.log(randomStartIndex);
+
+    let validStart = isHorizontal ? randomStartIndex <= width * width - ship.length ? randomStartIndex : 
+        width * width - ship.length :
+        // handle vertical
+        randomStartIndex <= width * width - width * ship.length ? randomStartIndex : 
+            randomStartIndex - ship.length * width + width;
 
     let shipBlocks = [];
 
     for (let i = 0; i < ship.length; i++) {
         if (isHorizontal) {
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i]);
         } else {
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i * width]);
         };
     };
 
-    shipBlocks.forEach(shipBlock => {
-        shipBlock.classList.add(ship.name);
-        shipBlock.classList.add('taken');
-    }); 
+    let valid
+
+    if (isHorizontal) {
+        shipBlocks.every((_shipBlock, index) =>
+            valid = shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)));
+        } else {
+            shipBlocks.every((_shipBlock, index) => 
+                valid = shipBlocks[0].id < 90 + (width * index + 1)
+            );
+        };
+
+    const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'));
+
+    if (valid && notTaken) {
+        shipBlocks.forEach(shipBlock => {
+            shipBlock.classList.add(ship.name);
+            shipBlock.classList.add('taken');
+        });
+    } else {
+        addShipPiece(ship);
+    };
+
 };
 
 ships.forEach(ship => addShipPiece(ship));
