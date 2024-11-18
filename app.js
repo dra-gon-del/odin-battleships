237c05fb430129cb410cff146122826a8ff8a9b5
A.Westwood <personal.email.aw1@gmail.com>
2024-11-18 13:34:25 +0000

    17. Fix boxing issue, random ship on computer board, addShipPiece funct.

--- app.js
+++ app.js
@@ -50,9 +50,27 @@ const carrier = new Ship('carrier', 5);
 
 const ships = [destroyer, submarine, cruiser, battleship, carrier];
 
-function addShipPiece() {
+function addShipPiece(ship) {
     const allBoardBlocks = document.querySelectorAll('#computer div');
-    console.log(allBoardBlocks);
+    let randomBoolean = Math.random() < 0.5;
+    let isHorizontal = randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+    console.log(randomStartIndex);
+
+    let shipBlocks = [];
+
+    for (let i = 0; i < ship.length; i++) {
+        if (isHorizontal) {
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+        } else {
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+        };
+    };
+
+    shipBlocks.forEach(shipBlock => {
+        shipBlock.classList.add(ship.name);
+        shipBlock.classList.add('taken');
+    }); 
 };
 
-addShipPiece();
+addShipPiece(destroyer);
